ad9fc1dd6a76a9517a5b6cf0ac29351f27e01a6f
Thomas Skerbis <thomas.skerbis@mac.com>
2018-01-10 08:49:09 +0100

    versionCompare hinzugefügt

--- update.php
+++ update.php
@@ -1,3 +1,6 @@
 <?php 
-$this->setConfig('suffix', '/');
-$this->setConfig('scheme', 'yrewrite_scheme_suffix');
+if (rex_string::versionCompare($this->getVersion(), '2.0', '<')) {
+    $this->setConfig('suffix', '/');
+    $this->setConfig('scheme', 'yrewrite_scheme_suffix');
+}
+
